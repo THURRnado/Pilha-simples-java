6773f9c823cae09ef88f3a6b88fa765b3687e941
Arthur Vieira <115512574+THURRnado@users.noreply.github.com>
2023-03-29 20:04:44 -0300

    Update MainPilha.java

--- MainPilha.java
+++ MainPilha.java
@@ -4,20 +4,27 @@ public class MainPilha {
 
 	public static void main(String[] args) {
 
+		//instanciando o objeto Pilha
 		Pilha p = new Pilha();
 		
+		//inserindo três elementos na pilha
 		p.insereTopo(1);
 		p.insereTopo(2);
 		p.insereTopo(3);
 		
+		//mostrando o valor presente no topo da lista 
 		System.out.println("Valor no topo da lista(antes da remoção): " + p.lerTopo());
 		
+		//removendo o valor qu está no topo
 		p.removeTopo();
 		
+		//mostrando novamente o valor presente no topo da lista
 		System.out.println("Valor no topo da lista(após a remoção): " + p.lerTopo());
 		
+		//mostrando se a lista está vazia
 		System.out.println("A pilha está vazia?: " + p.vazia());
 		
+		//retornando o tamanho da pilha
 		System.out.println("Tamanho da pilha: " + p.tamanho());
 	}
 
